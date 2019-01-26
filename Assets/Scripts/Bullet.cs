f13254c84a134b111059c252bef0938364a7fd12
alelievr <alelievr@student.42.fr>
2019-01-26 13:24:12 +0100

    Removed useless 2d template, added bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    SpriteRenderer  r;
+    ParticleSystem  particle;
+
+    void Start()
+    {
+        r = GetComponent< SpriteRenderer >();
+        particle = GetComponentInChildren< ParticleSystem >();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        r.enabled = false;
+        var e = particle.emission;
+        e.enabled = false;
+        GameObject.Destroy(gameObject, 5);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float            runSpeed = 1;
+    public float            fireTimeout = 0.5f;
+    public GameObject       bullet;
+    public Vector2          bulletForce = new Vector2(1, 1);
+
+    CharacterController2D   controller2D;
+    bool                    jump;
+    float                   move;
+    bool                    canFire = true;
+
+
+    void Start()
+    {
+        controller2D = GetComponent< CharacterController2D >();
+    }
+
+    private void Update()
+    {
+        move = Input.GetAxis("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Vector2 force = bulletForce;
+        if (!controller2D.m_FacingRight)
+            force.x = -force.x;
+
+        var g = GameObject.Instantiate(bullet, transform.position, Quaternion.identity);
+        var r = g.GetComponent< Rigidbody2D >();
+        r.AddForce(force, ForceMode2D.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        controller2D.Move(move * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
