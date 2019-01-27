83b5fe78d3c28ffa4d46ba5ab6458c22c89f752b
alelievr <alelievr@student.42.fr>
2019-01-27 01:06:17 +0100

    Added background musics

--- Assets/Scripts/DontPlayTheFirstTime.cs
+++ Assets/Scripts/DontPlayTheFirstTime.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 
 public class DontPlayTheFirstTime : MonoBehaviour
 {
-    public Behaviour       target;
+    static bool            active = false;
+    public Behaviour                    target;
 
-    void Start()
+    void Awake()
     {
+        if (active)
+            return ;
+        active = true;
         Debug.Log("Start unique !");
         DontDestroyOnLoad(gameObject);
         target.enabled = false;
+        SceneManager.sceneLoaded += (e, s) => {
+            GameObject g = GameObject.Find("TitleScreenVcam");
+            if (g != null)
+                g.SetActive(false);
+        };
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ### Font:
 https://www.fontsquirrel.com/fonts/Jinky
+
+### Musics:
+https://wingless-seraph.net/material-music_town_field.html A forgotten place
+https://wingless-seraph.net/material-music_horror.html A crime
